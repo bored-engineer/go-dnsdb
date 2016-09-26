3c1da771c111ea55587f7c5e3a60151d13122cba
Luke Young <luke@hydrantlabs.org>
2016-09-25 21:47:18 -0700

    Added Test_APIKeyTransport

---
+++ auth_test.go
@@ -0,0 +1,38 @@
+package dnsdb
+
+// Imports
+import (
+	"github.com/stretchr/testify/assert"
+
+	"net/http"
+	"testing"
+)
+
+// Make a test struct for testing
+type APIKeyTransportTest struct {
+	Request *http.Request
+}
+func (t *APIKeyTransportTest) RoundTrip(req *http.Request) (*http.Response, error) {
+	t.Request = req
+	return nil, nil
+}
+
+func Test_APIKeyTransport(t *testing.T) {
+	auth := APIKeyTransport{
+		APIKey: "d41d8cd98f00b204e9800998ecf8427e",
+	}
+	// Verify it returns a correct client
+	client := auth.Client()
+	assert.NotNil(t, client)
+	assert.Equal(t, client.Transport, &auth)
+	// Setup a test transport and send a request
+	testTransport := &APIKeyTransportTest{}
+	auth.Transport = testTransport
+	auth.RoundTrip(&http.Request{
+		Header: http.Header{
+			"X-Test": []string{"Header"},
+		},
+	})
+	assert.NotNil(t, testTransport.Request)
+	assert.Equal(t, testTransport.Request.Header.Get("X-API-Key"), "d41d8cd98f00b204e9800998ecf8427e")
+}
