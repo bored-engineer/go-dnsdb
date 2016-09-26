d8eeaf9e40663c2e38de779caa10d69e9fb7c30f
Luke Young <luke@hydrantlabs.org>
2016-09-25 22:12:07 -0700

    Improve rdata.go test coverage

--- rdata_test.go
+++ rdata_test.go
@@ -85,6 +85,17 @@ func Test_RDataService_LookupIP(t *testing.T) {
 	_, _, err = c.RData.LookupIP(net.ParseIP("104.244.13.104"), nil)
 	assert.NotNil(t, err)
 
+	// Verify that an invalid response fails
+	invalidServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, `{`)
+	}))
+	defer invalidServer.Close()
+	u, err = url.Parse(invalidServer.URL)
+	assert.Nil(t, err)
+	c.BaseURL = u
+	_, _, err = c.RData.LookupIP(net.ParseIP("104.244.13.104"), nil)
+	assert.NotNil(t, err)
+
 	// Verify that it gets and parses a response correctly
 	reportServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, `{"count":24,"time_first":1433550785,"time_last":1468312116,"rrname":"www.farsighsecurity.com.","rrtype":"A","rdata":"104.244.13.104"}
@@ -94,7 +105,9 @@ func Test_RDataService_LookupIP(t *testing.T) {
 	u, err = url.Parse(reportServer.URL)
 	assert.Nil(t, err)
 	c.BaseURL = u
-	actual, _, err := c.RData.LookupIP(net.ParseIP("104.244.13.104"), nil)
+	actual, _, err := c.RData.LookupIP(net.ParseIP("104.244.13.104"), &RDataLookupIPOptions{
+		RRType: "A",
+	})
 	assert.Nil(t, err)
 	assert.Equal(t, []RData{
 		RData{
@@ -132,6 +145,17 @@ func Test_RDataService_LookupIPNet(t *testing.T) {
 	_, _, err = c.RData.LookupIPNet(*ipnet, nil)
 	assert.NotNil(t, err)
 
+	// Verify that an invalid response fails
+	invalidServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, `{`)
+	}))
+	defer invalidServer.Close()
+	u, err = url.Parse(invalidServer.URL)
+	assert.Nil(t, err)
+	c.BaseURL = u
+	_, _, err = c.RData.LookupIPNet(*ipnet, nil)
+	assert.NotNil(t, err)
+
 	// Verify that it gets and parses a response correctly
 	reportServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, `{"count":24,"time_first":1433550785,"time_last":1468312116,"rrname":"www.farsighsecurity.com.","rrtype":"A","rdata":"104.244.13.104"}
@@ -142,7 +166,69 @@ func Test_RDataService_LookupIPNet(t *testing.T) {
 	u, err = url.Parse(reportServer.URL)
 	assert.Nil(t, err)
 	c.BaseURL = u
-	actual, _, err := c.RData.LookupIPNet(*ipnet, nil)
+	actual, _, err := c.RData.LookupIPNet(*ipnet, &RDataLookupIPNetOptions{
+		RRType: "A",
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, []RData{
+		RData{
+			Count:     Uint64(24),
+			TimeFirst: NewTimestamp(1433550785),
+			TimeLast:  NewTimestamp(1468312116),
+			RRName:    String("www.farsighsecurity.com."),
+			RRType:    String("A"),
+			RData:     String("104.244.13.104"),
+		},
+		RData{
+			Count:     Uint64(9429),
+			TimeFirst: NewTimestamp(1427897872),
+			TimeLast:  NewTimestamp(1468333042),
+			RRName:    String("farsightsecurity.com."),
+			RRType:    String("A"),
+			RData:     String("104.244.13.104"),
+		},
+	}, actual)
+}
+
+func Test_RDataService_LookupRaw(t *testing.T) {
+	// Setup a client
+	c := NewClient(nil)
+
+	// Verify that an error response fails
+	errorServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Oh No", 500)
+	}))
+	defer errorServer.Close()
+	u, err := url.Parse(errorServer.URL)
+	assert.Nil(t, err)
+	c.BaseURL = u
+	_, _, err = c.RData.LookupRaw([]byte{}, nil)
+	assert.NotNil(t, err)
+
+	// Verify that an invalid response fails
+	invalidServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, `{`)
+	}))
+	defer invalidServer.Close()
+	u, err = url.Parse(invalidServer.URL)
+	assert.Nil(t, err)
+	c.BaseURL = u
+	_, _, err = c.RData.LookupRaw([]byte{}, nil)
+	assert.NotNil(t, err)
+
+	// Verify that it gets and parses a response correctly
+	reportServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, `{"count":24,"time_first":1433550785,"time_last":1468312116,"rrname":"www.farsighsecurity.com.","rrtype":"A","rdata":"104.244.13.104"}
+{"count":9429,"time_first":1427897872,"time_last":1468333042,"rrname":"farsightsecurity.com.","rrtype":"A","rdata":"104.244.13.104"}
+`)
+	}))
+	defer reportServer.Close()
+	u, err = url.Parse(reportServer.URL)
+	assert.Nil(t, err)
+	c.BaseURL = u
+	actual, _, err := c.RData.LookupRaw([]byte("\x68\xF4\x0D\x68"), &RDataLookupRawOptions{
+		RRType: "A",
+	})
 	assert.Nil(t, err)
 	assert.Equal(t, []RData{
 		RData{
